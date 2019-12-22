d553814633145c4e122816348c5cbebe015ba28f
darsh-shah692 <darsh.shah_2017@sakec.ac.in>
2019-12-23 02:07:31 +0530

    added the shop page and also the header component that reamins constant throughout the flow of the site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import HomePage from './pages/homepages/homepage.component';
 import './pages/homepages/homepage.styles.scss';
+import Header from './components/header/header.component';
 
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
